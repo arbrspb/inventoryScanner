1c7bda11593d6d70faadbd355787a610fbc0186f
Artur <arbr@bk.ru>
2025-08-19 03:30:38 +0300

    работает но не плавный список

--- app/src/main/java/com/example/inventoryscanner/MainActivity.kt
+++ app/src/main/java/com/example/inventoryscanner/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+
 import androidx.compose.material3.*
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.*
@@ -27,6 +28,8 @@ import com.example.inventoryscanner.ui.theme.InventoryScannerTheme
 import com.journeyapps.barcodescanner.ScanContract
 import com.journeyapps.barcodescanner.ScanOptions
 
+//private val overscroll: Any
+//private val overscroll: Any
 private const val CAMERA_REQ = 123
 
 class MainActivity : ComponentActivity() {
